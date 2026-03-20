e177c9ec46e8353c83e015f8982107dbb5f77565
JefferH-PC <jhpcgamerbr177@gmail.com>
2026-03-20 17:16:47 -0300

    Exercício 5

---
+++ Todo Parte/Exercicios_Revisao/Exercicio5/src/TestaComputador.java
@@ -0,0 +1,6 @@
+public class TestaComputador {
+    public static void main (String[] args){
+        Computador computador = new Computador(101, "Dell", "Intel", "i7-12700K", 3.6f);
+        System.out.println(computador.toString());
+    }
+}
